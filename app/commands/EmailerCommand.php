757562c9e556d4826c2c49bc994bd0ec2d89b111
Andy Awidarto <andy.awidarto@gmail.com>
2014-09-23 10:02:31 +0700

    buyer fix

--- app/commands/EmailerCommand.php
+++ app/commands/EmailerCommand.php
@@ -81,7 +81,7 @@ class EmailerCommand extends Command {
 
                 $message->subject('Investors Alliance - E newsletter');
 
-                print_r($rec);
+                //print_r($rec);
 
                 if($rec['sentFromName'] == ''){
                     $message->from('info@propinvestorsalliance.com','Djonny Suwanto');
